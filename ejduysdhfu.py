2dc28f2d224ccbfe846c17f9557d04acff532142
Dell <kononenkos660@gmail.com>
2024-01-09 17:18:30 +0200

    3

--- ejduysdhfu.py
+++ ejduysdhfu.py
@@ -1,6 +1,14 @@
 #логування - вивід помилок через бібліотеку
 import logging
 #запис у файл
-logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w")
+logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode = "w")
 logging.debug("debug")
 logging.info("Інформація про рядок коду")
+logging.warning("Попередження про можливу помилку")
+logging.error("Помилка в програмі")
+logging.critical("Критична помилка")
+
+try:
+    print(10/0)
+except Exception:
+    logging.exception("Exeption")
